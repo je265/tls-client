9fe0e92592480b7fdc241eb3083549788af4a248
je265 <johnsonerik265@gmail.com>
2025-07-09 19:12:09 -0600

    stupid ass imports

--- tests/header_order_test.go
+++ tests/header_order_test.go
@@ -5,10 +5,10 @@ import (
 	"io"
 	"testing"
 
-	"github.com/bogdanfinn/tls-client/profiles"
+	"github.com/je265/tls-client/profiles"
 
 	http "github.com/bogdanfinn/fhttp"
-	tls_client "github.com/bogdanfinn/tls-client"
+	tls_client "github.com/je265/tls-client"
 	"github.com/stretchr/testify/assert"
 )
 
--- tests/random_extension_order_test.go
+++ tests/random_extension_order_test.go
@@ -3,13 +3,13 @@ package tests
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/bogdanfinn/tls-client/profiles"
+	"github.com/je265/tls-client/profiles"
 	"io"
 	"strings"
 	"testing"
 
 	http "github.com/bogdanfinn/fhttp"
-	tls_client "github.com/bogdanfinn/tls-client"
+	tls_client "github.com/je265/tls-client"
 	"github.com/stretchr/testify/assert"
 )
 
